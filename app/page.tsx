1c230dc31d5a3a7350090c439ac48f2dc692e042
Selcuk Cihan <selcukcihan@gmail.com>
2023-11-16 23:09:04 +0300

    Fix og tags

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,27 @@ import BooksComponent from '@/components/books'
 import Head from 'next/head'
 import { Title, Description } from '../core/meta'
 
+const bookshelf = getData()
+const metaDescription = Description + `Currently reading ${bookshelf.current[0].title} by ${bookshelf.current[0].author}`
+
+export const metadata = {
+  twitter: {
+    card: 'summary_large_image',
+    title: Title,
+    site: '@scihan',
+    description: metaDescription,
+    creator: '@scihan',
+    images: [`https://books.selcukcihan.com${bookshelf.current[0].cover}`],
+  },
+  openGraph: {
+    title: Title,
+    type: 'website',
+    url: 'https://books.selcukcihan.com',
+    description: metaDescription,
+    images: [`https://books.selcukcihan.com${bookshelf.current[0].cover}`],
+  },
+}
+
 function getData(): Bookshelf {
   const rawData = parse(fs.readFileSync('core/data.yaml', 'utf8'))
   const archive = rawData.archive.map((book: any) => ({
@@ -30,29 +51,10 @@ function getData(): Bookshelf {
 }
 
 export default function Page() {
-  const bookshelf = getData()
-  const metaDescription = Description + `Currently reading ${bookshelf.current[0].title} by ${bookshelf.current[0].author}}`
+
   return (
     <>
-      <Head>
-        <title>My Bookshelf</title>
-        <meta name="description" content={metaDescription} />
-        <link rel="icon" href="/favicon.ico" />
-        <meta property='og:title' content={Title} />
-        <meta property='og:description' content={metaDescription} />
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://books.selcukcihan.com' />
-        <meta property='og:image' content={`https://books.selcukcihan.com/${bookshelf.current[0].cover}`} />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:site' content='@scihan' />
-        <meta name='twitter:creator' content='@scihan' />
-        <meta name='twitter:title' content={Title} />
-        <meta name='twitter:description' content={metaDescription} />
-        <meta name='twitter:image' content={`https://books.selcukcihan.com/${bookshelf.current[0].cover}`} />
-      </Head>
-      <main>
-        <BooksComponent bookshelf={bookshelf} />
-      </main>
+      <BooksComponent bookshelf={bookshelf} />
     </>
   )
 }
